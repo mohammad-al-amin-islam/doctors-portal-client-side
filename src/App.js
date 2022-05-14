d27f7799112687cfab1509301233ed130a4945b1
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-05-14 12:49:59 +0600

    booking data added to db and initial query setup implemented

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './Pages/Login/Login';
 import ReqireAuth from './Pages/Login/ReqireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<SignUp />}></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Appoinment/BookingModal.js
+++ src/Pages/Appoinment/BookingModal.js
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const BookingModal = ({ treatment, date, setTreatment }) => {
@@ -8,9 +9,38 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
     const [user] = useAuthState(auth)
     const handleFrom = event => {
         event.preventDefault();
+        const bookingDate = format(date, 'PP');
         const slot = event.target.slot.value;
-        console.log(_id, name, slot);
-        setTreatment(null);
+        const booking = {
+            treatmentId: _id,
+            treatmentName: name,
+            date: bookingDate,
+            slot,
+            patientEmail: user.email,
+            patientName: user.displayName,
+            number: event.target.number.value
+        }
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    toast(`Appoinment is set, ${bookingDate} at ${slot}`);
+
+                }
+                else {
+                    toast.error(`Already have an appoinment ,${data.booking?.date} At ${data.booking?.slot}`);
+                }
+                setTreatment(null);
+            });
+
+
     }
     return (
         <div>
@@ -30,7 +60,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
                             }
                         </select>
                         <input name='name' type="text" disabled value={user?.displayName || ''} className="input input-bordered w-full" />
-                        <input name='number' type="text" placeholder="Phone Number" className="input input-bordered w-full" />
+                        <input name='number' type="text" placeholder="Phone Number" className="input input-bordered w-full" required />
                         <input name='email' type="email" disabled value={user?.email || ''} className="input input-bordered w-full" />
                         <input type="submit" value="Submit" className="btn btn-accent w-full " />
                     </form>
